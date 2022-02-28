b80cfbe8317b6dd4d9b8a5bf82175acdbc42aed8
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-28 17:37:17 -0500

    commit tree 921c89444a7cfdebfa093182bff769337ce980ed

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [moneyLeft, setMoneyLeft] = useState(100);
+  const [emptyPlates, setEmptyPlates] = useState([]);
+
+
+  useEffect(() => {
+    fetch(API)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const newSushis =data.map((sushi) => {
+          return {...sushi, eaten: false};
+        });
+        setSushis(newSushis);
+      })
+  }, []);
+
+  function spendMoney(wallet, cost, id) {
+    console.log("wallet:",wallet,"cost:",cost)
+    setMoneyLeft(wallet-cost);
+    setEmptyPlates([...emptyPlates,id]);
+    const updatedSushis = sushis.map((sushi) => {
+      if (sushi.id===id) {
+        return {...sushi, eaten:true};
+      } return {...sushi};
+    });
+    setSushis(updatedSushis)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} moneyLeft={moneyLeft} spendMoney={spendMoney}/>
+      <Table moneyLeft={moneyLeft} plates={emptyPlates}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, moneyLeft, spendMoney}) {
+  const [nextFour, setNextFour] = useState(0);
+  const sushisJsx = sushis.slice(nextFour,nextFour+4).map((sushi) => {
+    return (
+      <Sushi key={sushi.id} id={sushi.id} eaten={sushi.eaten} name={sushi.name} price={sushi.price} image={sushi.img_url} moneyLeft={moneyLeft} spendMoney={spendMoney} />
+    )
+  });
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushisJsx}
+      <MoreButton nextFour={nextFour} setNextFour={setNextFour}/>
     </div>
   );
 }
